5f6bc40fa920feb0cb48c6c967d7707479b26a44
Bhavani Prasad Sankara <inbhavas@symphonysummit.com>
2023-12-20 01:21:43 +0530

    debounceWithControls

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-const fetchWithTimeout = (url, duration) => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    let timerid = null;
-    
-    fetch(url, { signal })
-      .then((resp) => {
-      resp.json().then((e) => {
-        clearTimeout(timerid);
-        console.log("res",e)
-      }).catch((error) => {
-        console.log("error",error)
-      })
-     })
-    
-    timerid = setTimeout(() => {
-      console.log("Aborted");
-      controller.abort();
-    }, duration);
-}
-// fetchWithTimeout('https://jsonplaceholder.typicode.com/todos/1', 100).then((resp) => {
-//   console.log(resp);
-// }).catch((error) => {
-//   console.error(error);
-// });
+//classic debounce which executes after delay
+// const debounce = (func, delay) => {
+//   let inDebounce;
+//   return function() {
+//     const context = this;
+//     const args = arguments;
+//     clearTimeout(inDebounce);
+//     inDebounce = setTimeout(() => func.apply(context, args), delay);
+//   };
+// };
 
-// Aborted
-// error
+// this can invoke the func immediately & also after the delay
+const debounce = (fn, delay, option = { leading: true, trailing: true}) => {
+  let timeout;
+  let isLeadingInvoked = false;
+  //why we pass args & context here? 
+  //It passes the input target context from the eventlistener
+  //which you can use to log e.target.value
+  //this -> <input .../> element
+  //args -> keyboardEvent which contains target
+  return function (...args) {
+    const context = this;
+    
+    //base condition
+    if(timeout){
+      clearTimeout(timeout);
+    }
+    // handle leading
+    if(option.leading && !timeout){
+      fn.apply(context, args);
+      isLeadingInvoked = true;
+    }else{
+      isLeadingInvoked = false;
+    }
+    
+    // handle trailing
+    timeout = setTimeout(() => {
+      if(option.trailing && !isLeadingInvoked){
+        fn.apply(context, args);
+      }
+      
+      timeout = null;
+    }, delay);
+  }
+}
