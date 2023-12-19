1b56e6022183dd3ee02e02c601e9b808d0ac3be8
Bhavani Prasad Sankara <inbhavas@symphonysummit.com>
2023-12-19 23:39:51 +0530

    analyticsSDK

--- script.js
+++ script.js
@@ -1,42 +1,89 @@
 //source : Learners Bucket
-const circuitBreaker = (fn, failureCount, timeThreshold) => {
-  let failures = 0;
-  let timeSinceLastFailure = 0;
-  let isClosed = false;
-  
-  return function(){
-    // if service is closed, check if the threshold time exceeded to reset isClosed to false;
-    if(isClosed){
-      const diff = Date.now() - timeSinceLastFailure;
-      if(diff > timeThreshold){
-        isClosed = false;
+class SDK {
+  constructor(){
+    // hold the events
+    this.queue = [];
+
+    // track the count
+    this.count = 1;
+  }
+
+  // push event in the queue
+  log(ev) {
+    this.queue.push(ev);
+  }
+
+  // function to delay the exection
+  wait = () => new Promise((resolve, reject) => {
+    console.log(this.count)
+    setTimeout(() => {
+      // reject every n % 5 time
+      if(this.count % 5 === 0){
+        reject();
+      } else {
+        resolve();
       }
-      // else throw error
-      else{
-        console.error("Service unavailable");
-        return;
-      }
+    }, 1000);
+  });
+
+  // to send analytics
+  // recursively send the events
+  sendAnalytics = async function (){
+    // if there are no events in the queue
+    // stop execution
+    if(this.queue.length === 0){
+      return;
     }
-    
-    // try running the function
-    // if it passes reset the failure count
-    //use try to catch the manually sent errors
-    try{
-      const result = fn();
-      failures = 0;
-      return result;
-    }
-    // if function throws error / fails
-    // increase the failure count and 
-    // timewhen it fails
-    catch(error){
-      failures++;
-      timeSinceLastFailure = Date.now();
-      if(failures >= failureCount){
-        isClosed = true;  
-      }
-      
-      console.log("Error");
-    }
+
+    // get the first element from the queue
+    const current = this.queue.shift();
+
+    try {
+      // delay
+      console.log(current)
+      await this.wait();
+
+      // print the event
+      // can perform any other operations as well like making api call
+      console.log("Analytics sent " + current);
+
+      // increase the count
+      this.count++;
+    } catch(e){
+
+      // if exection fails
+      console.log("-----------------------");
+      console.log("Failed to send " + current);
+      console.log("Retrying sending " + current);
+      console.log("-----------------------");
+
+      // reset the count
+      this.count = 1;
+
+      // push the event back into the queue
+      this.queue.unshift(current);
+    }finally{
+
+      // recursively call the same function
+      // to send the remaining
+      this.sendAnalytics();
+    }	
   }
 }
+const sdk = new SDK();
+sdk.log("Event 1");
+sdk.log("Event 2");
+sdk.log("Event 3");
+sdk.log("Event 4");
+sdk.log("Event 5");
+sdk.log("Event 6");
+sdk.log("Event 7");
+sdk.log("Event 8");
+sdk.log("Event 9");
+sdk.log("Event 10");
+sdk.log("Event 11");
+sdk.log("Event 12");
+sdk.log("Event 13");
+sdk.log("Event 14");
+sdk.log("Event 15");
+sdk.sendAnalytics();
