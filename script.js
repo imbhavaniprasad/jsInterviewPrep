d59c6270dace79aca09e6f610e90a5143588838e
imbhavaniprasad <bhavaniprasadsmart@gmail.com>
2023-12-20 23:48:39 +0530

    remove unused cancel button

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 const fetchWithTimeout = (url, duration) => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    let timerid = null;
-    
-    fetch(url, { signal })
-      .then((resp) => {
+  const controller = new AbortController();
+  const signal = controller.signal;
+  let timerid = null;
+
+  fetch(url, { signal })
+    .then((resp) => {
       resp.json().then((e) => {
         clearTimeout(timerid);
-        console.log("res",e)
+        console.log("res", e)
       }).catch((error) => {
-        console.log("error",error)
+        console.log("error", error)
       })
-     })
-    
-    timerid = setTimeout(() => {
-      console.log("Aborted");
-      controller.abort();
-    }, duration);
-}
-// fetchWithTimeout('https://jsonplaceholder.typicode.com/todos/1', 100).then((resp) => {
-//   console.log(resp);
-// }).catch((error) => {
-//   console.error(error);
-// });
+    })
 
-// Aborted
-// error
+  timerid = setTimeout(() => {
+    console.log("Aborted");
+    controller.abort();
+  }, duration);
+}
