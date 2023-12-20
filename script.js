e34e34ff49c060ac8914081b9c478e26ff138cc4
imbhavaniprasad <bhavaniprasadsmart@gmail.com>
2023-12-21 00:27:01 +0530

    pipe-1

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const pipe = (...fns) => {
-  return function (val) {
-    for (let fn of fns) {
-      val = fn(val);
+const pipe = (obj) => {
+  return function (...args) {
+    for (let key in obj) {
+      let val = obj[key];
+      if (typeof val == "function") {
+        obj[key] = val(...args);
+      }
+      else obj[key] = pipe(val)(...args)
     }
-    return val;
+    //need to return the obj since this obj could be an object of objects/functions or just values
+    //which will assigned to else obj[key]
+    return obj;
   }
 }
