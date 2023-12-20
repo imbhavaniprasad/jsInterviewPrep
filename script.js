dd43f84b2c4f7be27432c8856631c8e9cb78cbab
imbhavaniprasad <bhavaniprasadsmart@gmail.com>
2023-12-20 23:55:53 +0530

    pipe-2

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-const fetchWithTimeout = (url, duration) => {
-  const controller = new AbortController();
-  const signal = controller.signal;
-  let timerid = null;
-
-  fetch(url, { signal })
-    .then((resp) => {
-      resp.json().then((e) => {
-        clearTimeout(timerid);
-        console.log("res", e)
-      }).catch((error) => {
-        console.log("error", error)
-      })
-    })
-
-  timerid = setTimeout(() => {
-    console.log("Aborted");
-    controller.abort();
-  }, duration);
+const pipe = (...fns) => {
+  return function (val) {
+    for (let fn of fns) {
+      val = fn(val);
+    }
+    return val;
+  }
 }
