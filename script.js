b81d48ff63b597721a563e65ea171cf3eda227ee
imbhavaniprasad <bhavaniprasadsmart@gmail.com>
2023-12-23 18:34:54 +0530

    aggregateArrayObjects

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-const pipe = (obj) => {
-  return function (...args) {
-    for (let key in obj) {
-      let val = obj[key];
-      if (typeof val == "function") {
-        obj[key] = val(...args);
+// const aggregateSimple = (arr) => {
+//   let data = new Map();
+//   let ans = arr.map((elem, index) => {
+//     if (!data.has(elem.user)) {
+//       data.set(elem.user, "");
+//       let x = {};
+//       x.user = elem.user;
+//       x.skills = [];
+//       arr.forEach(el => {
+//         if (el.user == elem.user) {
+//           x.skills.push(el.skill);
+//         }
+//       })
+//       return x;
+//     }
+//   })
+//   return ans.filter(e => e);
+// }
+const aggregate = (arr, on, who) => {
+  // using reduce() method to aggregate 
+  const agg = arr.reduce((a, b) => {
+    //a is accumulator (initial value as {})
+    //b is current value
+
+    const onValue = b[on];
+    const whoValue = b[who];
+
+    if (a[onValue]) {
+      a[onValue] = {
+        [on]: onValue,
+        [who]: [...a[onValue][who], whoValue]
       }
-      else obj[key] = pipe(val)(...args)
     }
-    //need to return the obj since this obj could be an object of objects/functions or just values
-    //which will assigned to else obj[key]
-    return obj;
-  }
+    else {
+      a[onValue] = {
+        [on]: onValue,
+        [who]: [whoValue]
+      }
+    }
+    return a;
+  }, {});
+  // since initial value was {} , answer would be a kinda Map
+  return Object.values(agg);
 }
