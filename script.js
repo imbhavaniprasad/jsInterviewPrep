66412ef9d6d524ea82add65d5d35954e7a9c1c23
imbhavaniprasad <bhavaniprasadsmart@gmail.com>
2023-12-25 15:59:10 +0530

    calculator

--- script.js
+++ script.js
@@ -1,24 +1,123 @@
-const timer = (init = 0, step = 1) => {
-  let intervalId;
-  let count = init;
-  //these 2 funcs are like forming closures
-  const startTimer = () => {
-    if (!intervalId) {
-      intervalId = setInterval(() => {
-        console.log(count);
-        count += step;
-      }, 1000);
+//Points to note :
+//substring takes start & endindex
+//substr takes first as start index & 2nd as count of chars
+
+//Where inputs are displayed
+const calcArea = document.querySelector("textarea");
+
+//Add input
+const addNums = text => {
+  let { value } = calcArea;
+
+  //Rules to add dot
+  const ruleA = value.length === 0 && text === ".";
+
+  //Add  only if both rules apply
+  if (!ruleA) {
+    calcArea.value += text;
+  }
+};
+
+//operators
+const oprList = ["+", "-", "*", "/", "%", "."];
+
+//Add operators
+const addOpr = text => {
+  const { value } = calcArea;
+  const lastCharacter = value[value.length - 1];
+
+  //Don't add repeated operators and initially without numbers
+  if (lastCharacter !== text) {
+    if (value.length > 0) {
+      calcArea.value += text;
     }
   }
 
-  const stopTimer = () => {
-    clearInterval(intervalId);
-    intervalId = null;
+  //If last character is operator then replace it with new operator
+  if (oprList.includes(lastCharacter)) {
+    calcArea.value = value.substr(0, value.length - 1) + text;
   }
-  //we could just invoke timer()() if we have just one return func()
-  // here we have 2 so return them to use invoke them seperately
-  return {
-    startTimer,
-    stopTimer,
-  };
+};
+
+const del = () => {
+  const { value } = calcArea;
+  if (value.length > 0) {
+    calcArea.value = value.substring(0, value.length - 1);
+  }
 }
+
+const clear = () => {
+  calcArea.value = "";
+}
+
+//Perform calculation
+const calc = () => {
+  const { value } = calcArea;
+  const result = eval(value);
+
+  if (!isNaN(result)) {
+    calcArea.value = result;
+  } else {
+    alert("Wrong expression, Please check your input");
+  }
+};
+//Add event listeners to the button
+//you can also use Array.from()
+document.querySelectorAll(".button-group > span").forEach(e => {
+  e.addEventListener("click", f => {
+    const { classList, innerText } = f.target;
+    console.log(innerText)
+    if (classList.contains("num")) {
+      //Number buttons clicked including .
+      addNums(innerText);
+    } else if (classList.contains("opr")) {
+      //Opertor buttons clicked
+      addOpr(innerText);
+    } else if (classList.contains("calc")) {
+      //Equal button clicked
+      calc();
+    } else if (classList.contains("delete")) {
+      //Backspace button clicked
+      del();
+    } else if (classList.contains("clear")) {
+      //Clear button clicked
+      clear();
+    }
+  });
+});
+
+
+//Add key events
+document.addEventListener("keydown", e => {
+  switch (e.key) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+    case ".":
+      addNums(e.key);
+      break;
+    case "/":
+    case "*":
+    case "+":
+    case "-":
+    case "%":
+      addOpr(e.key);
+      break;
+    case "Enter":
+      calc();
+      break;
+    case "Backspace":
+      del();
+      break;
+    case "c":
+      clear();
+    default:
+  }
+});
