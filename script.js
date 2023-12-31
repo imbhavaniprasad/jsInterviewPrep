6160533bbef4b2ba86f0734fdb1921043160ddc4
imbhavaniprasad <bhavaniprasadsmart@gmail.com>
2024-01-01 00:59:21 +0530

    tictactoe

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-const emojiEl = document.querySelectorAll(".far")
-const starEl = document.querySelectorAll(".fa-star");
-const colorsArray = ["red", "orange", "lightblue", "lightgreen", "voilet"];
-
-starEl.forEach((star, index) => {
-  star.addEventListener("click", () => {
-    updateRating(index);
+const box = document.querySelectorAll(".item");
+const winner = document.getElementById("winner");
+let playerA = true;
+let playerB = false;
+let fillCount = 0;
+box.forEach((item, idx) => {
+  item.addEventListener("click", function (e) {
+    if (playerA) {
+      item.innerHTML = "<b>X</b>"
+      playerA = false;
+      playerB = true;
+    }
+    else {
+      item.innerHTML = "<b>O</b>";
+      playerB = false;
+      playerA = true;
+    }
+    ++fillCount;
+    checkWinner();
   })
 })
 
-const updateRating = (index) => {
-  starEl.forEach((star, idx) => {
-    if (idx <= index) {
-      star.classList.add("active");
-    }
-    else star.classList.remove("active");
-  })
-
-  emojiEl.forEach((emoji, idx) => {
-    emoji.style.transform = `translateX(-${index * 50}px)`;
-    emoji.style.color = colorsArray[idx];
-  })
+const checkWinner = () => {
+  console.log(fillCount)
+  let check = playerA ? "OOO" : "XXX"
+  if ([box[0].textContent, box[1].textContent, box[2].textContent].join("") == check || [box[0].textContent, box[4].textContent, box[8].textContent].join("") == check
+    || [box[2].textContent, box[4].textContent, box[6].textContent].join("") == check || [box[0].textContent, box[3].textContent, box[6].textContent].join("") == check
+    || [box[2].textContent, box[5].textContent, box[8].textContent].join("") == check || [box[3].textContent, box[4].textContent, box[5].textContent].join("") == check
+    || [box[6].textContent, box[7].textContent, box[8].textContent].join("") == check || [box[1].textContent, box[4].textContent, box[7].textContent].join("") == "XXX") {
+    winner.textContent = `Player ${playerA ? "B" : "A"} Wins`
+    resetGame();
+  }
+  else if (fillCount == 9) {
+    winner.textContent = "Resetting"
+    resetGame();
+  }
+}
+const resetGame = () => {
+  setTimeout(() => {
+    box.forEach((item, idx) => {
+      item.innerHTML = "";
+      winner.textContent = "Winner is..."
+    })
+  }, 2000);
 }
